466ee516025db1e213ffd76b4000f261668dcb68
Fatmats <melikeats0561@gmail.com>
2021-02-05 18:51:41 +0300

    init

--- app.js
+++ app.js
@@ -1,50 +1,14 @@
-//TODO------------DOM-----------------------------
-//!----Element ekleme,attribute ekleme,chils ekleme------
-// const main = document.createElement("div")
-// main.setAttribute("class", "container list-group")
-// main.style.maxWidth = "300px"
-// main.style.marginTop = "10px"
-// document.body.appendChild(main);
+//TODO-----------------İnput verisi alma ----------------------------------------------
+// function myFunction() {
+//     const user = document.querySelector('input')
+//     const text = document.getElementById('write')
 
-// const a1 = document.createElement("a")
-// a1.setAttribute("class", "list-group-item list-group-item-action list-group-item-warning")
-// a1.setAttribute("aria-current", "true")
-// a1.innerText = "Lorem ipsuk dimsuk"
-// main.appendChild(a1)
+//     user.addEventListener("input",value);
 
-// const trash = document.createElement("button")
-// trash.setAttribute("class", "btn btn-info")
-// trash.innerText = "Delete"
-// trash.style.marginLeft="10px"
-// a1.appendChild(trash)
-
-// const a2 = document.createElement("a")
-// a2.setAttribute("class", "list-group-item list-group-item-action list-group-item-warning")
-// a2.innerText = "Lorem ipsuk dimsuk"
-
-// main.appendChild(a2)
-
-// const trash1 = document.createElement("button")
-// trash1.setAttribute("class", "btn btn-info")
-// trash1.innerText = "Delete"
-// trash1.style.marginLeft="10px"
-// a2.appendChild(trash1)
-
-// const a3 = document.createElement("a")
-// a3.setAttribute("class", "list-group-item list-group-item-action list-group-item-warning")
-// a3.innerText = "Lorem ipsuk dimsuk"
-// main.appendChild(a3)
-
-
-//TODO------------------Removing Element-----------------------------------------
-
-//  const list = document.querySelector("ul.list-group")
-// // el.remove();
-// const listitem= document.querySelectorAll("li.list-group-item")
-
-// del.addEventListener("click",()=>{list.removeChild(listitem[0])})
- 
-
-//TODO-----------------Local Storage----------------------------------------------
+//     function value(e) {
+//        const values= text.textContent=e.target.value
+//        values.localStorage
+//     }
+// }
 
 
